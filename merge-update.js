315b8ab9269f4ebfbc88328a784af7f440838e24
ajax <kolbaska205@gmail.com>
2018-07-05 11:23:56 +0600

    обновление старой таблицы (status)

--- merge-update.js
+++ merge-update.js
@@ -1,19 +1,36 @@
-var upd = new SCFile('infFilialsNew');
+var updateNewateNew = new SCFile('infFilialsNew');
 
 	for(var recordCount = 0; recordCount < vars['$action'].length; recordCount+=7){
-	print('start update');
-		upd.name = vars['$nval'][recordCount];
-		upd.dateCreated = vars['$nval'][recordCount+1];
-		upd.dateUpdated = vars['$nval'][recordCount+2];
-		upd.isActive = vars['$nval'][recordCount+3];
-		upd.id = vars['$nval'][recordCount+4];
-		upd.contact = vars['$nval'][recordCount+5];
-		//upd.id = vars['$nval'][recordCount+1];
-		upd.current.phase = vars['$nval'][recordCount+6];
-		print(upd);
-		var rc = upd.doInsert();
+	print('start updateNewate');
+		updateNew.name = vars['$nval'][recordCount];
+		updateNew.dateCreated = vars['$nval'][recordCount+1];
+		updateNew.dateupdateNewated = vars['$nval'][recordCount+2];
+		updateNew.isActive = vars['$nval'][recordCount+3];
+		updateNew.id = vars['$nval'][recordCount+4];
+		updateNew.contact = vars['$nval'][recordCount+5];
+		//updateNew.id = vars['$nval'][recordCount+1];
+		updateNew.current.phase = vars['$nval'][recordCount+6];
+		print(updateNew);
+		var rc = updateNew.doInsert();
 		if ( rc == RC_SUCCESS ){
 			print('ok');
 		}
 		//
+	}
+	var upd  = new SCFile('infFilials');
+	var sel = new SCFile('infFilials');
+	var query;
+	for(var fieldCount = 0; fieldCount <  fields['oldFields'].length; fieldCount+=7){
+		query = sel.doSelect('name='+vars['$oval'][fieldCount]);
+		if(query = RC_SUCCESS){
+			upd.name = vars['$oval'][fieldCount];
+			upd.dateCreated = vars['$oval'][fieldCount+1];
+			upd.dateUpdated = vars['$oval'][fieldCount+2];
+			upd.isActive = vars['$oval'][fieldCount+3];
+			upd.id = vars['$oval'][fieldCount+4];
+			upd.contact = vars['$oval'][fieldCount+5];
+			upd.current.phase = vars['$oval'][fieldCount+6];
+			upd.status = vars['$action'];
+			upd.doUpdate();
+		}
 	}
