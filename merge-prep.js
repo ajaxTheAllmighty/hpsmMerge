f91843f5ff14a51d968a3e47ea484aaf789e9f82
ajax <kolbaska205@gmail.com>
2018-07-05 17:59:49 +0600

    жду пока костя настроит отображение

--- merge-prep.js
+++ merge-prep.js
@@ -6,13 +6,16 @@ print('hello');
 var initalData = new SCFile('infFilials');
 var buffer = new SCFile('INFIntegrationBuffer');
 var bufferQuery;
-var initialQuery = initalData.doSelect('true');
+var initialQuery = initalData.doSelect('current.phase~="Work in progress"');
 if(initialQuery == RC_SUCCESS){
 	do{
 		print('add records');
 		buffer.cmdb_name = 'name';
 		buffer.cmdb_value = initalData['name'];
-		print(buffer.cmdb_name+' '+buffer.cmdb_value);
+		buffer.ci_name = initalData['current.phase'];
+		buffer.sccm_name = 'id';
+		buffer.sccm_value = initalData['id'];
+		buffer.status = system.functions.tod()+" "+system.functions.operator();
 		bufferQuery = buffer.doInsert();
 		if(bufferQuery == RC_SUCCESS){
 			print('insert ok');
