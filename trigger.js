0ca4303b1c3c93987ae46e5143a25838d6f63cfa
ajax <kolbaska205@gmail.com>
2018-07-05 12:18:41 +0600

    upd trigger

--- trigger.js
+++ trigger.js
@@ -1,2 +1,4 @@
-print('update trigger');
-record['status']+=system.functions.tod()+' '+system.functions.operator();
+if(! record['status']=='Не обработано'){
+	print('update trigger');
+	record['status']+=system.functions.tod()+' '+system.functions.operator();
+}
