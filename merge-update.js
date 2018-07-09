ec1748655e3116384755dfa07666fd479ea6b541
ajax <kolbaska205@gmail.com>
2018-07-09 14:17:51 +0600

    sdf

--- merge-update.js
+++ merge-update.js
@@ -2,16 +2,18 @@ var upd = new SCFile('INFIntegrationBuffer');
 var query;
 
 	for(var propCount = 0; propCount<vars['$action'].length(); propCount++){
-		if(propCount%7 == 0){
-			upd.ci_name = vars['$oval'][propCount];
+		if(vars['$action']!= 'new'){
+			if(propCount%7 == 0){
+				upd.ci_name = vars['$oval'][propCount];
+			}
+			upd.cmdb_name = vars['$ofield'][propCount];
+			upd.cmdb_value = system.functions.val(vars['$oval'][propCount],2);
+			upd.sccm_name = vars['$ofield'][propCount];
+			upd.sccm_value = system.functions.val(vars['$oval'][propCount],2);
+			//upd.id = propCount;
+			upd.status = vars['$action'][propCount];
+			upd.whoUpdated = system.functions.operator();
+			upd.dateUpdated =system.functions.tod();
+				query = upd.doInsert();
 		}
-		upd.cmdb_name = vars['$ofield'][propCount];
-		upd.cmdb_value = system.functions.val(vars['$oval'][propCount],2);
-		upd.sccm_name = vars['$ofield'][propCount];
-		upd.sccm_value = system.functions.val(vars['$oval'][propCount],2);
-		//upd.id = propCount;
-		upd.status = vars['$action'][propCount];
-		upd.whoUpdated = system.functions.operator();
-		upd.dateUpdated =system.functions.tod();
-			query = upd.doInsert();
 	}
